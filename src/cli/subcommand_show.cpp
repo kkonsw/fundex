ff3cba136406e2864a28fabf68fb5f0e7209d8e9
Konstantin Kuznetsov <kkonsw@gmail.com>
2021-01-28 18:38:10 +0300

    Add sort option for Show subcommand

--- src/cli/subcommand_show.cpp
+++ src/cli/subcommand_show.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 #include <memory>
+#include <stdexcept>
+#include <unordered_map>
 
 #include "cli/subcommand_show.h"
 #include "db/transaction_table.h"
@@ -10,24 +12,55 @@
 namespace fundex {
 
 /**
- * Function that runs code after Show subcommand is issued.
+ * Returns SortOrder based on string from user.
  *
+ * @param sort_option
+ *   Option passed by User.
+ * @return
+ *   SortOrder for Transactions.
+ * @note
+ *   Throws exception if user passed invalid option.
+ */
+SortOrder get_sort_order(const std::string &sort_option) {
+    SortOrder sort_order;
+    std::unordered_map<std::string, SortOrder> map = {
+        {"first", SortOrder::id},
+        {"last", SortOrder::id_desc},
+        {"old", SortOrder::date},
+        {"new", SortOrder::date_desc}
+    };
+
+    auto it = map.find(sort_option);
+    if (it == map.end()) {
+        throw std::runtime_error("Invalid sort option");
+    }
+
+    sort_order = it->second;
+    return sort_order;
+}
+
+/**
+ * Function that runs code after Show subcommand is issued.
  */
 static void run_subcommand_show(const SubcommandShowOptions& opt) {
-    TransactionTable transactions;
+    // Show most recent Transactions by default
+    SortOrder sort_order = SortOrder::date_desc;
+    if (!opt.sort_order.empty()) {
+        sort_order = get_sort_order(opt.sort_order);
+    }
+
     // Print all transactions
+    TransactionTable transactions;
     if (opt.show_all) {
-        auto all_transactions = transactions.get_transactions();
+        auto all_transactions = transactions.get_transactions(sort_order);
         print_transactions(all_transactions);
         return;
     }
 
-    // Print last n transactions
+    // Print n transactions
     int n = opt.num_records;
     if (n >= 0) {
-        auto last_transactions = transactions.get_transactions(n);
-        // Reverse vector to print more recent transactions last
-        std::reverse(last_transactions.begin(), last_transactions.end());
+        auto last_transactions = transactions.get_transactions(n, sort_order);
         print_transactions(last_transactions);
     }
 }
@@ -41,10 +74,10 @@ void setup_subcommand_show(CLI::App *app) {
             "Number of records to show")->excludes(show_all);
 
     std::string sort_help_msg = "Specify how to sort Transactions\n"
-        "\tid       show first added Transactions\n"
-        "\tid_d     show last added Transactions\n"
-        "\tdate     show oldest Transactions\n"
-        "\tdate_d   show most recent Transactions\n";
+        "\tfirst    show first added Transactions\n"
+        "\tlast     show last added Transactions\n"
+        "\told      show oldest Transactions\n"
+        "\tnew      show most recent Transactions\n";
     sub->add_option("-s,--sort", opt->sort_order, sort_help_msg);
     sub->callback([opt]() { run_subcommand_show(*opt); });
 }
